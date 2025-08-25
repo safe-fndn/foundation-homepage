309390ef6886683d69bd1b4b455fb7cdc91fd9e0
Rohan Sharma <rohansharma5494@gmail.com>
2025-08-25 17:02:47 +0530

    feat: add resources section

---
+++ src/content/resources.ts
@@ -0,0 +1,23 @@
+export const resourcesFeatures = [
+  {
+    name: "Multi-sig",
+  },
+  {
+    name: "Passkeys",
+  },
+  {
+    name: "Social Logins",
+  },
+  {
+    name: "Recovery",
+  },
+  {
+    name: "Modularity",
+  },
+  {
+    name: "Security",
+  },
+  {
+    name: "Open-source",
+  },
+];
