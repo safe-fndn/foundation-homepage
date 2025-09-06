180b226ece88b554d2ac6795301ae7fb2502e475
Rohan Sharma <rohansharma5494@gmail.com>
2025-09-07 02:09:19 +0530

    refactor: replace NextLink with useRouter and useSearchParams in ShowMoreButton for improved navigation handling

--- src/components/ShowMoreButton.tsx
+++ src/components/ShowMoreButton.tsx
@@ -1,26 +1,32 @@
 import { PAGE_QUERY_PARAM } from "@/lib/getPage";
-import NextLink from "next/link";
+import { useRouter, useSearchParams } from "next/navigation";
 import Button from "./ui/Button";
 
-const ShowMoreButton = ({ page }: { page: number }) => (
-  <div className="flex justify-center mt-[32px]">
-    <NextLink
-      href={{ query: { [PAGE_QUERY_PARAM]: page + 1 } }}
-      shallow
-      // Pagination marker for search engines
-      rel="next"
-    >
+const ShowMoreButton = ({ page }: { page: number }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleShowMore = () => {
+    const params = new URLSearchParams(searchParams);
+    params.set(PAGE_QUERY_PARAM, (page + 1).toString());
+
+    router.push(`?${params.toString()}`, { scroll: false });
+  };
+
+  return (
+    <div className="flex justify-center mt-[32px]">
       <Button
         variant="secondary"
         icon="./images/common/arrow-right-light.svg"
         iconAlt="arrow right"
         iconHeight={18}
         iconWidth={18}
+        onClick={handleShowMore}
       >
         Show more
       </Button>
-    </NextLink>
-  </div>
-);
+    </div>
+  );
+};
 
 export default ShowMoreButton;
