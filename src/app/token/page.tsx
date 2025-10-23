2dfcc229e3814322172b14c4bfceac8f0a248245
Rohan Sharma <rohansharma5494@gmail.com>
2025-10-24 04:09:27 +0530

    feat: Add token page layout and components

---
+++ src/app/token/page.tsx
@@ -0,0 +1,19 @@
+import HeroSection from "@/components/token/HeroSection";
+import Tokenomics from "@/components/token/Tokenomics";
+import WhatIsSafe from "@/components/token/WhatIsSafe";
+
+export default async function Home() {
+  return (
+    <div>
+      <div className="h-screen w-full relative">
+        <HeroSection />
+      </div>
+      <div className="pt-15 md:pt-[160px]">
+        <WhatIsSafe />
+      </div>
+      <div className="mt-[100px] md:mt-[160px] bg-[#E4EDE6] py-[100px] md:py-[160px]">
+        <Tokenomics />
+      </div>
+    </div>
+  );
+}
