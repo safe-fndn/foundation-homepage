402027bc24556036e3483368c78bf553fa9e0edf
Rohan Sharma <rohansharma5494@gmail.com>
2026-03-08 02:02:37 +0530

    feat: Add Problem component to Safenet page

--- src/app/safenet/page.tsx
+++ src/app/safenet/page.tsx
@@ -1,10 +1,14 @@
 import HeroSection from "@/components/safenet/HeroSection";
+import Problem from "@/components/safenet/Problem";
 import React from "react";
 
 export default function SafenetPage() {
   return (<main className="">
     <div className="bg-safenet-green">
       <HeroSection />
+    </div>
+    <div className="pt-[66px] max-w-[1440px] mx-auto sn-side-borders">
+      <Problem />
     </div>
   </main>)
 }
